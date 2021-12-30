979034264ce7204c9cb3470f9bf7aca96ca8ec22
kowalik-lukasz <exweap@gmail.com>
2021-12-30 14:53:51 +0100

    Add fitness function for the graph coloring problem

--- algorithms.py
+++ algorithms.py
@@ -1,17 +1,38 @@
+import numpy as np
+from utils import read_instance
+
+
 class Solve:
+    """
+    Utility for solving the provided problem instance and saving the results in the solved-instance directory.
+    The instance must exist within problem-instances directory
+    :param filename: the name of the problem instance
+    :param index_row: a flag indicating whether the first row contains headers (default: True)
+    :param index_col: a flag indicating whether the first column contains headers (default: False)
+    """
     def __init__(self, filename: str, index_row: bool = True, index_col: bool = False):
-        """
-        Displays the problem instance visually. The instance must exist within problem-instances directory
-        :param filename: the name of the problem instance
-        :param index_row: a flag indicating whether the first row contains headers (default: True)
-        :param index_col: a flag indicating whether the first column contains headers (default: False)
-        """
         self.filename = filename
-        self.index_row = index_row
-        self.index_col = index_col
+        self.incidence_matrix = read_instance(filename, index_row, index_col, output='numpy')
+
+    def _fitness_func(self, solution: np.ndarray = None):
+        fitness = 0
+        for i, node_incidence in enumerate(self.incidence_matrix):
+            inode_label = solution[i]
+            node_neighbors = solution[np.array(node_incidence, dtype=bool)]
+            conflicts = inode_label == node_neighbors
+            fitness += np.count_nonzero(conflicts)
+
+        return fitness
 
     def reference(self):
+        """
+        Solves the provided problem instance using the implemented reference algorithm.
+        """
         print("Solving with the reference algorithm...")
 
     def nature(self):
+        """
+        Solves the provided problem instance using the implemented nature-inspired PSO algorithm.
+        """
         print("Solving with the nature inspired algorithm...")
+        # self._fitness_func(np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
